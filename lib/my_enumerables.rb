ca76d1402e5777b9d752812a42261a85960ff877
Tom Fishwick <fishwickt@gmail.com>
2024-08-04 16:35:22 +0100

    my_none?

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -35,6 +35,15 @@ module Enumerable
     end
     return false
   end
+
+  def my_none?
+    self.each do |elem|
+      if yield(elem)
+        return false
+      end
+    end
+    return true
+  end
 end
 
 # You will first have to define my_each
